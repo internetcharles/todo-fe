2b91963f5aa4d18acc8735e6bd454368eb1dda7c
internetcharles <charlesodellsmith@gmail.com>
2020-08-19 17:02:20 -0700

    things are back on one page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 import React, { Component } from 'react';
 import './App.css';
-import { Link, Switch, Route, BrowserRouter as Router, Redirect } from 'react-router-dom';
+import { Switch, Route, BrowserRouter as Router, Redirect } from 'react-router-dom';
 import AuthPage from './AuthPage';
-import ListPage from './ListPage';
 import HomePage from './HomePage';
-import CreatePage from './CreatePage';
+import Header from './Header';
 
 export default class App extends Component {
   state = {
@@ -19,21 +18,30 @@ export default class App extends Component {
     console.log(this.state.token)
   }
 
+  clearToken = () => {
+    this.setState({ token: '' });
+
+    localStorage.setItem('token', '');
+  }
+
   render() {
   return (
     <div className="App">
                 <Router>
+                  <Header />
                   {
                     !this.state.token ? <Redirect to='/login' /> : <></>
                   }
-                  <Link to='/login'>Login</Link>
-                  <Link to='/list'>List</Link>
-                  <Link to='/create'>Create</Link>
+                  {
+                    this.state.token ? <button onClick={this.clearToken}>Logout</button> : null
+                  }
                     <Switch>
                         <Route 
                             path="/" 
                             exact
-                            render={(routerProps) => <HomePage token={this.state.token} {...routerProps} />} 
+                            render={(routerProps) => <HomePage token={this.state.token}
+                            handleToken={this.handleToken}
+                             {...routerProps} />} 
                         />
                         <Route 
                             path="/login" 
@@ -41,20 +49,6 @@ export default class App extends Component {
                             render={(routerProps) => <AuthPage 
                             token={this.state.token}
                             handleToken={this.handleToken}
-                                {...routerProps} />} 
-                        />
-                        <Route 
-                            path="/list" 
-                            exact
-                            render={(routerProps) => <ListPage 
-                            token={this.state.token}
-                                {...routerProps} />} 
-                        />
-                                                <Route 
-                            path="/create" 
-                            exact
-                            render={(routerProps) => <CreatePage 
-                            token={this.state.token}
                                 {...routerProps} />} 
                         />
                     </Switch>
